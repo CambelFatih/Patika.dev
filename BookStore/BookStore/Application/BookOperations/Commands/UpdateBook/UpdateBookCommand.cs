16df2177b9a51273ae65c12934d5842323d19c43
Fatih Furkan Çambel <cambelfatih01@gmail.com>
2023-10-20 20:15:17 +0300

    BookStore

---
+++ BookStore/BookStore/Application/BookOperations/Commands/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.Commands.BookOperations.UpdateBook
+{
+    public class UpdateBookCommand{
+        
+        public UpdateBookModel Model { get; set; }
+        public int BookId { get; set; }
+        private readonly IBookStoreDbContext _context;
+        public UpdateBookCommand(IBookStoreDbContext context)
+        {
+            _context=context;
+        }
+        public void Handle(){
+            var book = _context.Books.SingleOrDefault(x => x.Id == BookId);
+            if(book is null)
+            {
+                throw new InvalidOperationException("Kitap Bulunamadi");
+            }
+            book.Title=Model.Title!=default ? Model.Title: book.Title;
+            book.GenreId=Model.GenreId!=default?Model.GenreId: book.GenreId;
+            _context.SaveChanges();
+        }
+    }
+    public class UpdateBookModel{
+        public string Title { get; set; }
+        public int GenreId { get; set; }
+    }
+}
