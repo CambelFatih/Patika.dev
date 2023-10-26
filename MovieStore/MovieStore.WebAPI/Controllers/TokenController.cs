91a13d072857475796d605d2bdf9b67d3663e51d
Fatih Furkan Çambel <cambelfatih01@gmail.com>
2023-10-26 21:22:37 +0300

    Initial Commit

---
+++ MovieStore/MovieStore.WebAPI/Controllers/TokenController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.App.CustomerOperations.Commands.TokenOperations;
+using WebApi.DbOperations;
+using WebApi.TokenOperations.Model;
+
+namespace Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IMovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+        readonly IConfiguration _configuration;
+
+        public TokenController(IMovieStoreDbContext context, IMapper mapper, IConfiguration configuration)
+        {
+            _context = context;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        [HttpPost("connect/token")]
+        public IActionResult CreateToken([FromBody] CreateTokenModel login)
+        {
+            CreateTokenCommand command = new CreateTokenCommand(_context, _mapper, _configuration);
+            command.Model = login;
+            Token token = command.Handle();
+            return Ok(token);
+        }
+
+        [HttpGet("refreshToken")]
+        public IActionResult RefreshToken([FromQuery] string token)
+        {
+            RefreshTokenCommand command = new RefreshTokenCommand(_context, _configuration);
+            command.RefreshToken = token;
+            var resultToken = command.Handle();
+
+            return Ok(resultToken);
+        }
+    }
+}
